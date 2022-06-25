e6f6f3e5a229b25e01cf2efbf9095c52cdc53a49
florianrohrauer <89992758+florianrohrauer@users.noreply.github.com>
2022-06-25 18:08:41 +0000

    Deploying to gh-pages from @ florianrohrauer/florianrohrauer.github.io@94dc4cee6b95702c349e973fcf940f25c67684f8 🚀

---
+++ _next/static/fR0tETu6sXLRWVg1D0DDw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/652-bd8b55eafe5c42f8.js","static/chunks/pages/index-93e3ca7487f2bae6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
